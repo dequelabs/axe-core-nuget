4daadf6e11923f2c3b93f3411711aa43aef59d97
Dan Bjorge <danielbj@microsoft.com>
2022-08-22 18:06:07 -0400

    feat(commons): create initial commons test project

---
+++ packages/commons/test/StubTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Deque.AxeCore.Commons.Test {
+    public class StubTests
+    {
+        [Test]
+        public void EmptyStubTestShouldBeRunnable()
+        {
+            Assert.Pass();
+        }
+    }
+}
