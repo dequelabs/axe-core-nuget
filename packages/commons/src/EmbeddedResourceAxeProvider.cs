3b71618d091a0b77afc469afc37768cec741d2e2
JGibson2019 <jacqueline.gibson@microsoft.com>
2022-08-30 12:41:29 -0700

    Copy embedded resource providers to commons

---
+++ packages/commons/src/EmbeddedResourceAxeProvider.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Deque.AxeCore.Commons
+{
+    internal class EmbeddedResourceAxeProvider : IAxeScriptProvider
+    {
+        public string GetScript() => EmbeddedResourceProvider.ReadEmbeddedFile("axe.min.js");
+    }
+}
---
+++ packages/commons/src/EmbeddedResourceProvider.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using System.Text;
+
+namespace Deque.AxeCore.Commons
+{
+    internal static class EmbeddedResourceProvider
+    {
+        public static string ReadEmbeddedFile(string fileName)
+        {
+            var assembly = typeof(EmbeddedResourceProvider).Assembly;
+            var resourceStream = assembly.GetManifestResourceStream($"Deque.AxeCore.Common.Resources.{fileName}");
+            using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
