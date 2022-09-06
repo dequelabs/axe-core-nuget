980af6416d8edef51b05539ab28f93ec1999d134
JGibson2019 <jacqueline.gibson@microsoft.com>
2022-09-06 14:07:11 -0700

    Simple test for bundled; invalid case for embedded

--- packages/commons/test/BundledAxeScriptProviderTest.cs
+++ packages/commons/test/BundledAxeScriptProviderTest.cs
@@ -6,20 +6,29 @@ using System;
 
 namespace Deque.AxeCore.Commons.Test
 {
-    // [TestFixture]
-    // [NonParallelizable]
-    // public class BundledAxeScriptProviderTest
-    // {
-    //     // private static Mock<EmbeddedResourceProvider> mock = new Mock<EmbeddedResourceProvider>();
-    //     // [Test]
-    //     // public void GetScriptCalled() 
-    //     {
-    //         // mock.Setup(mock => mock.GetScript())
-    //         //     .Returns("foo string")
-    //         //     .Verifiable();
-    //         // var testBundledProvider = new BundledAxeScriptProvider();
-    //         // var scriptResult = testBundledProvider.GetScript();
-    //         // mock.Verify(mock => mock.GetScript(), Times.Once);
-    //     }
-    // }
+    [TestFixture]
+    [NonParallelizable]
+    public class BundledAxeScriptProviderTest
+    {
+        // private static Mock<EmbeddedResourceProvider> mock = new Mock<EmbeddedResourceProvider>();
+        private static BundledAxeScriptProvider testProvider = new BundledAxeScriptProvider();
+        [Test]
+        public void GetScriptCalled() 
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var readResult = testProvider.GetScript();
+                readResult.Should().NotBeNull();
+            });
+            // var getScriptCalled = readResult != null ? readResult : null;
+            // Console.WriteLine(getScriptCalled);
+            // getScriptCalled.Should().NotBeNull();
+            // mock.Setup(mock => mock.GetScript())
+            //     .Returns("foo string")
+            //     .Verifiable();
+            // var testBundledProvider = new BundledAxeScriptProvider();
+            // var scriptResult = testBundledProvider.GetScript();
+            // mock.Verify(mock => mock.GetScript(), Times.Once);
+        }
+    }
 }
--- packages/commons/test/EmbeddedResourceProviderTest.cs
+++ packages/commons/test/EmbeddedResourceProviderTest.cs
@@ -11,9 +11,18 @@ namespace Deque.AxeCore.Commons.Test
     {
         private static readonly string sampleFileName = "sampleFile.txt";
         [Test]
-        public void PassedValidString() {
+        public void PassedValidFileName() {
             var readResult = EmbeddedResourceProvider.ReadEmbeddedFile(sampleFileName);
             readResult.Should().Be("sample output");
+        }
+
+        [Test]
+        public void PassedInvalidFileName() {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var readResult = EmbeddedResourceProvider.ReadEmbeddedFile("sampleFile.html");
+                readResult.Should().NotBeNull();
+            });
         }
     }
 }
