4ae24960c70451babd5e2cc59cf42f3de12f1d0c
JGibson2019 <jacqueline.gibson@microsoft.com>
2022-09-08 11:28:55 -0700

    move EmbeddedResourceProvider functionality into BundledAxeScriptProvider; update FileAxeScriptProvider error logic

--- packages/commons/src/BundledAxeScriptProvider.cs
+++ packages/commons/src/BundledAxeScriptProvider.cs
@@ -1,8 +1,20 @@
+using System.IO;
+using System.Text;
 
 namespace Deque.AxeCore.Commons
 {
     public class BundledAxeScriptProvider : IAxeScriptProvider
     {
-        public string GetScript() => EmbeddedResourceProvider.ReadEmbeddedFile("axe.min.js");
+        public static string ReadEmbeddedFile(string manifestResourceName)
+        {
+            var assembly = typeof(BundledAxeScriptProvider).Assembly;
+            var resourceStream = assembly.GetManifestResourceStream(manifestResourceName);
+            using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public string GetScript() => ReadEmbeddedFile($"Deque.AxeCore.Commons.Resources.axe.min.js");
     }
 }
