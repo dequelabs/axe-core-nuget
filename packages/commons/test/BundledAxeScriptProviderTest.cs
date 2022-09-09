3e79fa8300fb7889b34355b266698ca096ff4bd1
Lisa Li <lisli@microsoft.com>
2022-09-09 12:11:29 -0700

    change path strings to local variables and remove unneeded package

--- packages/commons/test/BundledAxeScriptProviderTest.cs
+++ packages/commons/test/BundledAxeScriptProviderTest.cs
@@ -2,7 +2,6 @@ using NUnit.Framework;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Deque.AxeCore.Commons;
-using Moq;
 using FluentAssertions;
 using System;
 using System.IO;
@@ -13,28 +12,29 @@ namespace Deque.AxeCore.Commons.Test
     [TestFixture]
     public class BundledAxeScriptProviderTest
     {
-        private static readonly string testContext = TestContext.CurrentContext.TestDirectory.ToString();
-        private readonly string basePath = testContext.Split(new string[] { "test" }, StringSplitOptions.None)[0];
+        
         private static readonly string copyrightHeader = "* Your use of this Source Code Form is subject to the terms of the Mozilla Public\n * License, v. 2.0.";
         [Test]
         public void GetScriptCalled()
         {
             var scriptProvider = new BundledAxeScriptProvider();
-            Assert.DoesNotThrow(() =>
-            {
-                string readResult = scriptProvider.GetScript();
-                string axeVersion = "axe v" + getAxeVersion();
 
-                readResult.Should().NotBeNull();
-                Assert.That(readResult, Contains.Substring(axeVersion));
-                Assert.That(readResult, Contains.Substring(copyrightHeader));
-            });
+            string readResult = scriptProvider.GetScript();
+            string axeVersion = "axe v" + getAxeVersion();
+
+            readResult.Should().NotBeNull();
+            Assert.That(readResult, Contains.Substring(axeVersion));
+            Assert.That(readResult, Contains.Substring(copyrightHeader));
         }
 
         private string getAxeVersion()
         {
+            string testContext = TestContext.CurrentContext.TestDirectory.ToString();
+            string basePath = testContext.Split(new string[] { "test" }, StringSplitOptions.None)[0];
             string absolutePackageJsonPath = Path.Combine(basePath, "src", "package.json");
+
             JObject packageJson = JObject.Parse(File.ReadAllText(absolutePackageJsonPath));
+
             string axeVersion = (string)packageJson["dependencies"]["axe-core"];
             string regex = "(\\d+\\.)?(\\d+\\.)?(\\d+)";
             axeVersion = Regex.Match(axeVersion, regex).ToString();
