5d382019b080e52268421ab1bf28baf6960948b1
Adam Cutler <adam.r.cutler@gmail.com>
2022-09-28 09:55:11 -0400

    Remove debug line

--- packages/playwright/src/Resources/runPartial.js
+++ packages/playwright/src/Resources/runPartial.js
@@ -2,6 +2,5 @@
     const context = typeof contextObj === 'string' ? JSON.parse(contextObj) : contextObj;
     const options = JSON.parse(optionsStr);
     return axe.runPartial(context || document, options)
-        .then(res => {window.res = res; return res;})
         .then((results) => JSON.stringify(results));
 }
