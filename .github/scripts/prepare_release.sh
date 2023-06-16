725b25319434b2680a6f2d1378d8ae96bd3cf182
Adam Cutler <adam.r.cutler@gmail.com>
2023-06-16 11:15:48 -0400

    ci: auto patch-release every 2 weeks (#87)

--- .github/scripts/prepare_release.sh
+++ .github/scripts/prepare_release.sh
@@ -6,9 +6,9 @@ set -e
 releaseLevel="$1"
 
 oldVersion="$(node -pe 'require("./package.json").version')"
-npx commit-and-tag-version@"$COMMIT_AND_TAG_VERSION_VERSION" --release-as "$releaseLevel" --skip.commit=true --skip.changelog=true --skip.tag=true
+npx commit-and-tag-version --release-as "$releaseLevel" --skip.commit=true --skip.changelog=true --skip.tag=true
 newVersion="$(node -pe 'require("./package.json").version')"
 
 sed -i -e "s/<VersionPrefix>$oldVersion<\/VersionPrefix>/<VersionPrefix>$newVersion<\/VersionPrefix>/" ./packages/*/src/*.csproj
 
-npx conventional-changelog-cli@"$CHANGELOG_VERSION" -p angular -i CHANGELOG.md -s
+npx conventional-changelog-cli -p angular -i CHANGELOG.md -s
