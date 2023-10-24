dc5b8e1af4076dfbcfe01435a1404691ba5c7739
AdnoC <adam.r.cutler@gmail.com>
2023-10-24 13:26:40 -0400

    playwright is signed

--- packages/playwright/src/AssemblyInfo.cs
+++ packages/playwright/src/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -16,5 +17,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM.
 
 [assembly: Guid("0f3f7b6c-4f37-4130-a99f-6df34f7d293e")]
-[assembly: InternalsVisibleTo("Deque.AxeCore.Playwright.Test")]
+[assembly:AssemblyKeyFileAttribute("../../../axe-core.snk")]
+[assembly: InternalsVisibleTo("Deque.AxeCore.Playwright.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100157ae9fcc643b5fef7b2a49c32072ae7c9edfa3f8b21d415d2dffd552f4b6267e805d9a650aaeb38214978a09aebc7a507e201680b59553144dab81b92b6c3d14a60de3e61e6b073666950a66769b8f2d05c9fcf2e0476b5f7746e3431f67a8177d75a5370401ffbc14da8b4a6af7829787a8beddec09e2b6bbfcba287e1f9a7")]
 [assembly: InternalsVisibleTo("Moq, PublicKey=00240000048000009400000006020000002400005253413100040000010001008d6f3bf78d32913a1ebc51a2b847653857601ea9d4afd9950523010df835af240dcf8780f6f3c1b92162378352e1291d199579e1b8e5ea7397226dbfe8ce70ca3d9634a710aebf2b53f0fd4a656ab04dde575c2d88daaff7ab9bd75b3991803846f64db14640ea7c3dee6891db172bb8235ce055242261315db58b830674b0aa")]
